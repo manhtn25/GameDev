bf18d20e40bd20bc4b6149e8c5a0bff2d90f950d
Michelle Cao <cao.michelle27@gmail.com>
2022-10-04 15:49:15 -0500

    Added Sound Effects

--- tartarus_Compound/Assets/tartarusCompound/Scripts/Punch.cs
+++ tartarus_Compound/Assets/tartarusCompound/Scripts/Punch.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Punch : MonoBehaviour
 {
+    public AudioClip punching;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy"))
         {
+            AudioSource.PlayClipAtPoint(punching, transform.position);
             Destroy(collision.gameObject);
            
         }
