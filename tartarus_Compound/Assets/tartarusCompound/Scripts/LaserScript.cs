c8f3cdb558972b6503239c9540dbd485e2525132
JRII_Ataka <jrejasii@gmail.com>
2022-10-30 22:13:50 -0500

    made some changes

--- tartarus_Compound/Assets/tartarusCompound/Scripts/LaserScript.cs
+++ tartarus_Compound/Assets/tartarusCompound/Scripts/LaserScript.cs
@@ -12,14 +12,17 @@ public class LaserScript : MonoBehaviour
     [SerializeField] private Interactables virtualCheck;
     [SerializeField] private Interactables virtualCheckTwo;
 
+    [SerializeField] private GameObject puzzleSecondCoin;
+    [SerializeField] private GameObject puzzleFirstCoin;
+
     // Start is called before the first frame update
     void Start()
     {
-       coll = GetComponent<BoxCollider2D>();
-       anim = GetComponent<Animator>(); //animator component
-                                        //inVirtual = GetComponent<Interactables>().inVirtual;
+        coll = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>(); //animator component
+                                         //inVirtual = GetComponent<Interactables>().inVirtual;
 
-      //terminalListSize = virtualCheck.Length;
+        //terminalListSize = virtualCheck.Length;
 
     }
 
@@ -30,57 +33,78 @@ public class LaserScript : MonoBehaviour
         if (virtualCheck.inVirtual == false && virtualCheckTwo.inVirtual == false)
         {
             anim.SetBool("isOn", true);
-            if (tag == "LaserTrap")
-            {
-                coll.enabled = true;
-            }
-            else
+
+            coll.enabled = true;
+
+            /*else
             {
                 coll.enabled = false;
-            }
+            }*/
         }
         else if (virtualCheck.inVirtual == true || virtualCheckTwo.inVirtual == true)
         {
-            anim.SetBool("isOn", false);
-
             if (tag == "LaserTrap")
             {
+                anim.SetBool("isOn", false);
                 coll.enabled = false;
             }
-            else
+        }
+
+        if (puzzleSecondCoin.GetComponent<SpriteRenderer>().enabled == false && puzzleFirstCoin.GetComponent<SpriteRenderer>().enabled == false)
+        {
+            if (tag == "ElectricTrap")
             {
-                coll.enabled = true; //this is referring to electricTrap
+                anim.SetBool("isOn", false);
+                coll.enabled = false;
             }
 
         }
 
 
-       /* if (virtualCheckTwo.inVirtual == false)
-        {
-            anim.SetBool("isOn", true);
-            if (tag == "LaserTrap")
-            {
-                coll.enabled = true;
-            }
-            else
-            {
-                coll.enabled = false;
-            }
-        }
-        else if (virtualCheckTwo.inVirtual == true)
-        {
-            anim.SetBool("isOn", false);
 
-            if (tag == "LaserTrap")
-            {
-                coll.enabled = false;
-            }
-            else
-            {
-                coll.enabled = true; //this is referring to electricTrap
-            }
+        /* else if (virtualCheck.inVirtual == true || virtualCheckTwo.inVirtual == true)
+         {
+             anim.SetBool("isOn", false);
 
-        }*/
+             if (tag == "LaserTrap")
+             {
+                 coll.enabled = false;
+             }
+             else
+             {
+                 coll.enabled = true; //this is referring to electricTrap
+             }
+
+         //just add different animation state later when wanting off and on electric + laser
+         }*/
+
+
+        /* if (virtualCheckTwo.inVirtual == false)
+         {
+             anim.SetBool("isOn", true);
+             if (tag == "LaserTrap")
+             {
+                 coll.enabled = true;
+             }
+             else
+             {
+                 coll.enabled = false;
+             }
+         }
+         else if (virtualCheckTwo.inVirtual == true)
+         {
+             anim.SetBool("isOn", false);
+
+             if (tag == "LaserTrap")
+             {
+                 coll.enabled = false;
+             }
+             else
+             {
+                 coll.enabled = true; //this is referring to electricTrap
+             }
+
+         }*/
 
 
 
@@ -89,6 +113,6 @@ public class LaserScript : MonoBehaviour
 
 
     }
-    
+
 }
 
